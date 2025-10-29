bf9b5383e6f979e571cdbfe6f63b55f2103c0443
Danish2606 <23035007@myrp.edu.sg>
2025-10-29 15:43:15 +0800

    database

--- db.js
+++ db.js
@@ -1,24 +1,16 @@
 const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-<<<<<<< HEAD
 // Database connection details
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Republic_c207',
-    database: 'C300_RPecoJourney'
-=======
-//Database connection details
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'RPEcoJourney',
-    port: '3316'
->>>>>>> 565b31a5a070fdf4968dd1ebfc402e121646a5f0
-  });
+    port: 3316
+});
 
-//Connecting to database
+// Connecting to database
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
