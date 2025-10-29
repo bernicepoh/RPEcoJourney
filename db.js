c60ea737c069e51b4a45cc7b9a3c88d6a94d17a3
Danish2606 <23035007@myrp.edu.sg>
2025-10-29 15:29:34 +0800

    dB.JS

---
+++ db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+// Database connection details
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Republic_c207',
+    database: 'C300_RPecoJourney'
+  });
+
+//Connecting to database
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+module.exports = db;
