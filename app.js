4eb837f90f65450ac2d7618ce7998f8f8a71835d
bernicepoh <23018838@myrp.edu.sg>
2025-11-10 22:07:48 +0800

    content - read, add

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require('express');
-
-const userController = require('./controllers/userController');
-const productController = require('./controllers/productController');
-const catController = require('./controllers/catController');
-const orderController = require('./controllers/orderController');
-const cartController = require('./controllers/cartController');
+const contentController = require('./controller/contentController');
 
 const multer = require('multer');
 const ses = require('express-session');
@@ -22,7 +17,6 @@ const storage = multer.diskStorage({
     }
 });
 
-
 // Set up view engine
 app.set('view engine', 'ejs');
 //  enable static files
@@ -32,100 +26,24 @@ app.use(express.urlencoded({
     extended: false
 }));
 
-
-
 const upload = multer({ storage: storage });
-
-
-// Session Middleware
-app.use(ses({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    // Cookies expires after 1 week of inactivity
-    cookie: {maxAge: 1000 * 60 *60 * 24 * 7}
-}));
 
 app.use(flash());
 
-app.get('/', productController.getProduct);
-app.get('/products', productController.getProducts);
-app.get('/product/:id', productController.getproductId);
-app.get('/editproduct/:id', productController.editproduct);
-app.post('/editproduct/:id',upload.single('image'), productController.editproductForm);
-app.get('/addproduct', productController.getproductForm);
+ 
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.get('/categories', catController.getCat);
-app.post('/addReview',catController.addReview);
-app.get('/review/:productId', catController.getOrderItems);
-app.get('/categoriesAdmin', catController.getCatA);
-app.get('/editCategories/:categoryId', catController.Cat);
-app.post('/editCategories/:categoryId', upload.single('categoryImage'), catController.editCat);
-app.get('/deleteCategory/:categoryId', catController.delCat);
-app.get('/addCategories', catController.getAdd);
-app.post('/addCategories', upload.single('categoryImage'), catController.addCat);
+app.get('/category', (req, res) => {
+    res.render('category');
+});
 
-// Route to delete a specific item from the cart
-app.get('/DeleteCartItem/:id', cartController.DeleteCartItem);
-
-// Home route (Display Shopping Cart)
-app.get('/', cartController.GetCartItems);
-
-// Debugging: Logs for cartController
-console.log(cartController);
-console.log(cartController.GetCartItems);
-console.log(cartController.DeleteCartItem);
-app.get('/DeleteCartItem/:id', cartController.DeleteCartItem);
-
-// Home route (Display Shopping Cart)
-app.get('/shoppingcart', cartController.GetCartItems);
-
-// Define routes
-app.get('/order', orderController.getOrdersUser);
-
-app.get('/card', orderController.getCard);
-
-app.get('/paynow', orderController.getPaynow);
-
-app.get('/paypal', orderController.getPaypal);
-
-app.get('/orderPlaced', orderController.getorderPlaced);
-
-app.get('/cancelOrder/:id', orderController.cancelOrder);
-
-app.get('/deliveryStatus', orderController.getdeliveryStatus);
-
-app.get('/editAddress/:userId', orderController.editAddressForm);
-
-app.post('/editAddress/:userId', orderController.editAddress);
-
-
-// Define Login Page Route
-app.get('/', userController.getAboutPage);
-// Define Register Page Route
-app.get('/register', userController.getRegister);
-app.post('/register', userController.RegisterUser);
-// Login route to render login page
-app.get("/login", userController.getLoginPage);
-// Login route for form submission
-app.post("/login", userController.LoginUser);
-// User profile page route
-app.get('/profile', userController.getProfile);
-// Edit Profile Form routes
-app.get('/editProfile/:id', userController.getEditProfile);
-app.post('/editProfile/:id', upload.single("image"), userController.editProfile);
-// Logout route
-app.get('/logout', userController.Logout);
-// Users database page routes
-app.get('/user', userController.getUsers);
-app.get('/user/:id', userController.getUser);
-app.get('/user/:id/delete', userController.deleteUser);
-//  Admin database page routes
-app.get('/addAdmin', userController.getAddAdmin);
-app.post('/addAdmin', userController.addAdmin)
-app.get('/admin', userController.getAdmins);
-app.get('/admin/:id', userController.getAdmin);
-app.get('/admin/:id/delete', userController.deleteAdmin);
+// Content Routes
+app.get('/category/:id/content', contentController.getContentByCategory);
+app.get('/content/:id', contentController.getContent);
+app.get('/addContent', contentController.addContentForm);
+app.post('/addContent', upload.single('contentFile'), contentController.addContent);
 
 // Connect to PORT
 const PORT = process.env.PORT || 3000;
--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
-const mysql = require('mysql2');
+
 
 // Database connection details
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'RPEcoJourney',
-    port: 3316
+    database: 'rpecojourney'
 });
 
 // Connecting to database
