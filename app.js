bdad3978f93819eb292804fab410e1834834154c
bernicepoh <23018838@myrp.edu.sg>
2026-01-27 12:56:19 +0800

    change port running

--- app.js
+++ app.js
@@ -208,5 +208,6 @@ app.get('/401', (req, res) => {
 
 // Start express server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
+});
