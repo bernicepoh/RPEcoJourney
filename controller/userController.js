1ed2ff11d9c307c217a65ff35e43ebf6a6a6858b
bernicepoh <23018838@myrp.edu.sg>
2026-01-27 15:37:03 +0800

    removed "" from userID

--- controller/userController.js
+++ controller/userController.js
@@ -37,7 +37,7 @@ exports.login = (req, res) => {
       req.flash('loginSuccess', 'Login successful');
  
       const initProgress = `
-        INSERT INTO "user" ("userID", "totalXP", level, streak, "CheckInDate")
+        INSERT INTO "user" (userID, totalXP, level, streak, CheckInDate)
         VALUES ($1, 0, 1, 0, NULL)
         ON CONFLICT ("userID") DO NOTHING
       `;
