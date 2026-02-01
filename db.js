c335f57c320fcbb544429acd05a5032239a0eb72
bernicepoh <23018838@myrp.edu.sg>
2026-02-02 01:29:21 +0800

    fixed digital signage using hosted db

--- db.js
+++ db.js
@@ -1,25 +1,14 @@
 const mysql = require('mysql2');
 const fs = require('fs');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'rpecojourney'
-  });
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '4000'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: process.env.DB_CA ? { ca: fs.readFileSync(process.env.DB_CA) } : undefined
+});
 
 // Test on startup
 db.connect((err) => {
